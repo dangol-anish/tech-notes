ef0f48d5bd72c5d554e3bf5f3be7c7b493a510c3
Anish Dangol <dangol.anish001@gmail.com>
2024-01-10 21:42:51 +0545

    added logger

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const { resolveSoa } = require("dns");
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
 
 app.use(express.json());
 
