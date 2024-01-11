7d1bbecb87ffbd9a790d6b66c8e1b8002c48ddd9
Anish Dangol <dangol.anish001@gmail.com>
2024-01-11 11:07:37 +0545

    connected mongo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const { resolveSoa } = require("dns");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const corsOptions = require("./config/corsOptions");
 const cors = require("cors");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 
@@ -36,4 +42,15 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
