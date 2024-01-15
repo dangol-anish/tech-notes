38b3af4c30040b6371a09a09cd0251b9b413f31a
Anish Dangol <dangol.anish001@gmail.com>
2024-01-15 19:51:32 +0545

    added logic for form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,13 @@ import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
-
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Routes>
@@ -20,12 +21,16 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="dash" element={<DashLayout />}>
             <Route index element={<Welcome />} />
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-            </Route>
 
             <Route path="users">
               <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
             </Route>
           </Route>
           {/*End Dash*/}
--- client/src/features/users/NewUserForm.jsx
+++ client/src/features/users/NewUserForm.jsx
@@ -1,6 +1,74 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useAdd } from "../notes/notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { useAddNewUserMutation } from "./usersApiSlice";
+import { ROLES } from "../../config/roles";
+
+const USER_REGEX = /^[A-z]{3,20}$/;
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const NewUserForm = () => {
+  const [addNewUser, { isLoading, isSuccess, isError, error }] =
+    useAddNewUserMutation();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(["Employee"]);
+
+  useEffect(() => {
+    setValidUsername(USER_REGEX.test(username));
+  }, [username]);
+
+  useEffect(() => {
+    setValidPassword(PWD_REGEX.test(password));
+  }, [password]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setUsername("");
+      setPassword("");
+      setRoles([]);
+      navigate("/dash/users");
+    }
+  }, [isSuccess, navigate]);
+
+  const onUsernameChanged = (e) => setUsername(e.target.value);
+  const onPasswordChanged = (e) => setPassword(e.target.value);
+
+  const onRolesChanged = (e) => {
+    const values = Array.from(
+      e.target.selectedOptions, //HTMLCollection
+      (option) => option.value
+    );
+    setRoles(values);
+  };
+
+  const canSave =
+    [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+
+  const onSaveUserClicked = async (e) => {
+    e.preventDefault();
+    if (canSave) {
+      await addNewUser({ username, password, roles });
+    }
+  };
+
+  const options = Object.values(ROLES).map((role) => {
+    return (
+      <option key={role} value={role}>
+        {" "}
+        {role}
+      </option>
+    );
+  });
+
   return <div>NewUserForm</div>;
 };
 
