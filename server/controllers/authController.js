f1998cb2ac1b880e08ec3d88ffdda3c91f605b8e
Anish Dangol <dangol.anish001@gmail.com>
2024-01-17 14:36:16 +0545

    added login auth with jwt

---
+++ server/controllers/authController.js
@@ -0,0 +1,117 @@
+const asyncHandler = require("express-async-handler");
+const pool = require("../config/dbConn");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// @desc Login
+// @route POST /auth
+// @access Public
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+
+  const foundUser = result.rows[0];
+
+  console.log(foundUser);
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match) return res.status(401).json({ message: "Unauthorized" });
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        username: foundUser.username,
+        roles: foundUser.roles,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "30s" }
+  );
+
+  const refreshToken = jwt.sign(
+    { username: foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "30s",
+    }
+  );
+
+  // Create secure cookie with refresh token
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, //accessible only by web server
+    // secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 30000, //cookie expiry: set to match rT
+  });
+
+  // Send accessToken containing username and roles
+  res.json({ accessToken });
+});
+
+// @desc Refresh
+// @route GET /auth/refresh
+// @access Public - because access token has expired
+const refresh = (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) return res.status(401).json({ message: "Unauthorized" });
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: "Forbidden" });
+
+      const result = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [decoded.username]
+      );
+
+      const foundUser = result.rows[0];
+      if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
+
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username,
+            roles: foundUser.roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10s" }
+      );
+
+      res.json({ accessToken });
+    })
+  );
+};
+
+// @desc Logout
+// @route POST /auth/logout
+// @access Public - just to clear cookie if exists
+const logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None" });
+  res.json({ message: "Cookie cleared" });
+};
+
+module.exports = {
+  login,
+  refresh,
+  logout,
+};
